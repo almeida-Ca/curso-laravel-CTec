baf4d32ed46c49270a36f891a6267cd65e55325c
Cairo <cairodealmeida@gmail.com>
2025-02-09 23:52:07 -0300

    implementacao modal

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,23 +1,34 @@
 <template>
- 
-
-  <div>
-    <h1>Bem-vindo à minha aplicação Vue!</h1>
-    <p>{{ message }}</p>
-  </div>
-
   
-
-
+  <h1>Bem-vindo à minha aplicação Vue!</h1>
+  <p>Clique no Botao para receber uma mensagem</p>
+  <Button label="Clique" style="width: 60px; height: 30px; border: 2px solid ;" @click="openModal"/>
+  <ListItem :visible="isModalOpen" @close="closeModal"/>
 
 </template>
+
+
 
 <script setup>
 
   import { ref, reactive } from 'vue';
   import ListItem from './Components/listItem.vue';
+  import Button from 'primevue/button';
+  import { Dialog } from 'primevue';
 
-  const message = ref('Bem-vindo')
+  const mensagemDoPai = "Olá filho";
+  const isModalOpen = ref(false);
+
+
+  //Funcao para abrir o modal
+  const openModal = () => {
+    isModalOpen.value = true;
+  }
+
+  //Funcao para fechar o modal
+  const closeModal = () => {
+    isModalOpen.value = false;
+  }
 
 
 </script>
--- resources/js/Components/listItem.vue
+++ resources/js/Components/listItem.vue
@@ -1,13 +1,38 @@
 <template>
-
-  
-  
-
+    <div class="card flex justify-center" v-if="visible">
+      <Dialog modal header="Edit Profile" :style="{ width: '25rem' }">
+        <p>{{ message }}</p>
+        <Button label="Close" style="width: 60px; height: 30px; border: 2px solid ;" @click="closeModal"/>
+      </Dialog>
+    </div>
 </template>
 
 <script setup>
+  import { ref } from "vue";
+  import { defineProps, defineEmits } from "vue";
+  import { Dialog, Button } from "primevue";
+  
+  //Definindo props visible recebida do componente pai
+  const props = defineProps({
+    message: String,
+    visible: Boolean
+  });
+
+
+  //Declarando o evento close para emitir quando o modal for fechado
+  const emit = defineEmits(['close']);
+
+  //Funcao para emitir o evento close para o pai
+  const closeModal = () => {
+    emit('close');
+  }
+
 
 </script>
+
+  
+
+
 
 <style scoped>
 
